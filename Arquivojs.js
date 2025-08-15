95c645ef327dcea72fe4268f1be7d7ed36866538
eduardoguimaraes-coder <eduardo.guimaraes@dcomp.ufs.br>
2025-08-15 12:58:43 -0300

    Update Arquivojs.js

--- Arquivojs.js
+++ Arquivojs.js
@@ -1,12 +1,6 @@
+import {Holw, Howler} from 'howler'
 
-const lista1 = ['up','down']
-const lista2 = ['up','left','down','lower']
-//COMPARANDO 2 LISTAS, E CRIANDO OUTRA COM ELEMENTOS IGUAIS
-const resultado = lista1.filter((item, indice) => {
-    if (item === lista2[indice]) { 
-        return item;
-    } else {
-        return false;
-    }
-})
-console.log(resultado)
+const botões = () => {
+    const botaopause = document.querySelector('#pause')
+}
+const inicial = {play: true, score: 0, tela: 'start'}
